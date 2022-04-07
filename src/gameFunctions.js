6e65ee38c3dcc92a1bb38cac2490d4ccf639495f
sydney.murray <sydney_i_murray@yahoo.co.uk>
2022-04-07 18:39:25 +0100

    The winnerCheck function is complte

--- src/gameFunctions.js
+++ src/gameFunctions.js
@@ -2,9 +2,10 @@ function handleClick(event){
     const eventClass = event.target.getAttribute("class")
     if (!eventClass.includes("main-board__block")) return
     
-    if(!checkMove(eventClass.slice(eventClass.length-1))) return
+    if(!checkMoveIsLegal(eventClass.slice(eventClass.length-1))) return
     
-    if (winnerCheck()) return
+    winnerCheck()
+    if (winner) return
     
     if (player === "X") {
         player = "O"
@@ -24,34 +25,48 @@ function clearTheBoard(){
         block.setAttribute("class","main-board__block main-board__block-" +
             blocksClass.slice(blocksClass.length-1))
     })
+    winner = ""
     player = "X"
     notificationH2.innerText = "It's the Cross's turn to play" 
 }
 
 function winnerCheck(){
-    if (checkRow("a","b","c")) highlightWinningBlocksRed("a","b","c")
+    if (checkRow("a","b","c")) highlightWinningRowRed("a","b","c")
+    if (checkRow("a","e","i")) highlightWinningRowRed("a","e","i")
+    if (checkRow("a","d","g")) highlightWinningRowRed("a","d","g")
+    if (checkRow("b","e","h")) highlightWinningRowRed("b","e","h")
+    if (checkRow("c","f","i")) highlightWinningRowRed("c","f","i")
+    if (checkRow("c","e","g")) highlightWinningRowRed("c","e","g")
+    if (checkRow("d","e","f")) highlightWinningRowRed("d","e","f")
+    if (checkRow("g","h","i")) highlightWinningRowRed("g","h","i")
 
-    
+    if (winner ==="X") notificationH2.innerText = "The Cross's have won" 
+    if (winner ==="O") notificationH2.innerText = "The Naught's have won"     
 }
 
-function setWinningRedText(letter){
-    let block = document.querySelector(".main-board__block-" + letter)
-    block.setAttribute("class", "red-text main-board__block main-board__block-" + letter)
+function highlightWinningRowRed(blockOne, blockTwo, blockThree){
+    let block = document.querySelector(".main-board__block-" + blockOne)
+    block.setAttribute("class", "red-text main-board__block main-board__block-" + blockOne)
+    
+    block = document.querySelector(".main-board__block-" + blockTwo)
+    block.setAttribute("class", "red-text main-board__block main-board__block-" + blockTwo)
+    
+    block = document.querySelector(".main-board__block-" + blockThree)
+    block.setAttribute("class", "red-text main-board__block main-board__block-" + blockThree)
+    console.log(`Winning  ${blockOne} ${blockTwo} ${blockThree} `)
 }
 
 function checkRow(blockOne, blockTwo, blockThree){
     let blockOneInnerText = document.querySelector(".main-board__block-" + blockOne).innerText
     let blockTwoInnerText = document.querySelector(".main-board__block-" + blockTwo).innerText
     let blockThreeInnerText = document.querySelector(".main-board__block-" + blockThree).innerText
+
     if (blockOneInnerText === blockTwoInnerText && blockOneInnerText === blockThreeInnerText)
-        winner = blockOneInnerText     
+        winner = blockOneInnerText 
+    return winner    
 }
 
-function highlightWinningBlocksRed(blockOne, blockTwo, blockThree){
-
-}
-
-function checkMove(block){
+function checkMoveIsLegal(block){
     const blockToBeChecked = document.querySelector(".main-board__block-"+block)
     if (blockToBeChecked.innerText === player){  
         alert("You have already selected that block!")
